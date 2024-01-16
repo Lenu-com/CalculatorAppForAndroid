785db6af8803c3719c19c90774f5176680351dcd
Lenu-com <oretyan@icloud.com>
2024-01-16 22:56:48 +0900

    リファクタリング実施

--- Calculator/app/src/main/java/to/msn/wings/calculator/MainActivity.java
+++ Calculator/app/src/main/java/to/msn/wings/calculator/MainActivity.java
@@ -12,83 +12,103 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    /* 数字ボタンのオブジェクトを取得する際の定数を宣言 */
-    private static final int BUTTON_COUNT = 9;
-
-    private Button btn_AC;
-    private Button btn_PM;
-    private Button btn_per;
-    private Button btn_div;
-    private Button btn_7;
-    private Button btn_8;
-    private Button btn_9;
-    private Button btn_mul;
-    private Button btn_4;
-    private Button btn_5;
-    private Button btn_6;
-    private Button btn_sub;
-    private Button btn_1;
-    private Button btn_2;
-    private Button btn_3;
-    private Button btn_add;
-    private Button btn_0;
-    private Button btn_dot;
-    private Button btn_equ;
+    private static final int NUMBER_BUTTON_TOTAL = 9;
     private TextView textView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* 数字ボタンオブジェクトを取得 */
-        for (int i = 0; i <= BUTTON_COUNT; i++) {
-            int buttonId = getResources().getIdentifier("btn_" + i, "id", getPackageName());
-            Button button = findViewById(buttonId);
-
-            /* 数字ボタンにクリックリスナーを設定 */
-            setClickListener(button, String.valueOf(i));
-        }
-
-        /* 数字ボタン以外のオブジェクトを取得 */
-        btn_AC = findViewById(R.id.btn_AC);
-        btn_PM = findViewById(R.id.btn_PM);
-        btn_per = findViewById(R.id.btn_per);
-        btn_div = findViewById(R.id.btn_div);
-        btn_mul = findViewById(R.id.btn_mul);
-        btn_sub = findViewById(R.id.btn_sub);
-        btn_add = findViewById(R.id.btn_add);
-        btn_dot = findViewById(R.id.btn_dot);
-        btn_equ = findViewById(R.id.btn_equ);
-
         /* テキストビューオブジェクトを取得 */
         textView = findViewById(R.id.textView);
 
+        View.OnClickListener allClearClickListener = view -> {
+            /* TODO: ACボタンが押された時の処理を実装 */
+        };
 
+        View.OnClickListener clearClickListener = view -> {
+            /* TODO: Cボタンが押された時の処理を実装 */
+        };
 
-        /* クリックリスナーの設定 */
-        setClickListener(btn_AC, "AC");
-        setClickListener(btn_PM, "+/-");
-        setClickListener(btn_per, "%");
-        setClickListener(btn_div, "÷");
-        setClickListener(btn_mul, "×");
-        setClickListener(btn_sub, "-");
-        setClickListener(btn_add, "+");
-        setClickListener(btn_dot, ".");
-        setClickListener(btn_equ, "=");
+        View.OnClickListener signClickListener = view -> {
+            /* TODO: +/-ボタンが押された時の処理を実装 */
+        };
+
+        View.OnClickListener percentClickListener = view -> {
+            /* TODO: %ボタンが押された時の処理を実装 */
+        };
+
+        View.OnClickListener dotClickListener = view -> {
+            /* TODO: .ボタンが押された時の処理を実装 */
+        };
+
+        View.OnClickListener equalClickListener = view -> {
+            /* TODO: =ボタンが押された時の処理を実装 */
+        };
+
+        /* ボタンオブジェクトを取得 */
+        setupOperatorButtons();
+        setupNumberButtons();
+        Button btn_AC = findViewById(R.id.btn_AC);
+        Button btn_PM = findViewById(R.id.btn_PM);
+        Button btn_dot = findViewById(R.id.btn_dot);
+        Button btn_equ = findViewById(R.id.btn_equ);
+
+        /* ボタンにリスナーを設定 */
+        btn_AC.setOnClickListener(allClearClickListener);
+        btn_PM.setOnClickListener(signClickListener);
+        btn_dot.setOnClickListener(dotClickListener);
+        btn_equ.setOnClickListener(equalClickListener);
     }
 
-    private void setClickListener(Button button, String buttonText) {
-        /* ボタンがクリックされたときの処理 */
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* クリックされたボタンのテキストを表示領域に追加 */
-                appendToDisplay(buttonText);
+    private void setupNumberButtons() {
+        int[] numberButtonIds = {
+                R.id.btn_0,
+                R.id.btn_1,
+                R.id.btn_2,
+                R.id.btn_3,
+                R.id.btn_4,
+                R.id.btn_5,
+                R.id.btn_6,
+                R.id.btn_7,
+                R.id.btn_8,
+                R.id.btn_9
+        };
+
+        View.OnClickListener numberClickListener = view -> {
+            String buttonText = ((Button) view).getText().toString();
+            String displayText = textView.getText().toString();
+            if (buttonText.equals("0") && displayText.equals("0")) {
+                return;
             }
-        });
+            if (displayText.equals("0")) {
+                textView.setText("");
+            }
+            appendToDisplay(buttonText);
+        };
+
+        for (int id : numberButtonIds) {
+            findViewById(id).setOnClickListener(numberClickListener);
+        }
+    }
+
+    private void setupOperatorButtons() {
+        int[] operatorButtonIds = {
+                R.id.btn_div,
+                R.id.btn_mul,
+                R.id.btn_sub,
+                R.id.btn_add
+        };
+
+        View.OnClickListener operatorClickListener = view -> {
+            /* TODO: 演算子ボタンが押された時の処理を実装
+              (前述のように演算子毎にクリックリスナーを定義するのもあり) */
+        };
+
+        for (int id : operatorButtonIds) {
+            findViewById(id).setOnClickListener(operatorClickListener);
+        }
     }
 
     private void appendToDisplay(String text) {
