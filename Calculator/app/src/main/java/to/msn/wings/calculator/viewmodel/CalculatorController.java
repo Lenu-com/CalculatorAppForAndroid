de54c81be63d62b15597442bfa0f4c2a34a3f8c3
Lenu-com <oretyan@icloud.com>
2024-02-17 11:41:49 +0900

    四則演算の優先順位を考慮した設計に変更

--- Calculator/app/src/main/java/to/msn/wings/calculator/viewmodel/CalculatorController.java
+++ Calculator/app/src/main/java/to/msn/wings/calculator/viewmodel/CalculatorController.java
@@ -6,13 +6,24 @@ import androidx.lifecycle.MutableLiveData;
 import to.msn.wings.calculator.model.CalculatorLogic;
 
 public class CalculatorController {
-    public enum Operator {
-        ADD, SUB, MUL, DIV, NONE
+    enum Operator {
+        ADD, SUB, MUL, DIV, NONE;
+
+        public boolean isHighPriority() {
+            return this == MUL || this == DIV;
+        }
+
+        public boolean isNone() {
+            return this == NONE;
+        }
     }
+
     private final MutableLiveData<String> displayText = new MutableLiveData<>();
-    private Operator selectedOperator = Operator.NONE;
-    private double storedNumber = 0.0;
     private final CalculatorLogic calculatorLogic = new CalculatorLogic();
+    private double accumulator = 0.0;
+    private double currentNumber = 0.0;
+    private Operator currentOperator = Operator.NONE;
+    private Operator lastOperator = Operator.NONE;
 
     public CalculatorController() {
         displayText.setValue("0");
@@ -27,26 +38,6 @@ public class CalculatorController {
             return String.valueOf((int) number);
         }
         return String.valueOf(number);
-    }
-
-    private void updateSelectedOperator(String operatorButtonText) {
-        if (operatorButtonText == null) {
-            return;
-        }
-        switch (operatorButtonText) {
-            case "+":
-                selectedOperator = Operator.ADD;
-                break;
-            case "-":
-                selectedOperator = Operator.SUB;
-                break;
-            case "×":
-                selectedOperator = Operator.MUL;
-                break;
-            case "÷":
-                selectedOperator = Operator.DIV;
-                break;
-        }
     }
 
     private void updateDisplayNumber(String numberText) {
@@ -64,82 +55,17 @@ public class CalculatorController {
         displayText.setValue(currentDisplayText + numberText);
     }
 
-    private void appendDotToDisplayNumber() {
-        String currentDisplayText = displayText.getValue();
-        if (currentDisplayText == null) {
-            return;
-        }
-        if (currentDisplayText.contains(".")) {
-            return;
-        }
-        displayText.setValue(currentDisplayText + ".");
+    private void preformCalculationAndUpdateDisplayNumber() {
     }
 
     private void currentNumberClear() {
         displayText.setValue("0");
     }
 
-    private void allClear() {
-        displayText.setValue("0");
-        selectedOperator = Operator.NONE;
-        storedNumber = 0.0;
-    }
-
-    private void changeSignOfDisplayNumber() {
-        String currentDisplayText = displayText.getValue();
-        if (currentDisplayText == null) {
-            return;
-        }
-        double currentNumber = Double.parseDouble(currentDisplayText);
-        double signChangedNumber = calculatorLogic.toggleSign(currentNumber);
-        displayText.setValue(formattedNumberText(signChangedNumber));
-    }
-
-    private void preformCalculationAndUpdateDisplayNumber() {
-        String currentDisplayText = displayText.getValue();
-        if (currentDisplayText == null) {
-            return;
-        }
-        double currentNumber = Double.parseDouble(currentDisplayText);
-        double result = calculatorLogic.calculate(storedNumber, currentNumber, selectedOperator);
-        displayText.setValue(formattedNumberText(result));
-        selectedOperator = Operator.NONE;
-    }
-
-    public void onAllClearButtonClicked() {
-        allClear();
-    }
-
     public void onNumberButtonClicked(String numberButtonText) {
-        if (selectedOperator != Operator.NONE) {
+        if (currentOperator.isNone()) {
             currentNumberClear();
         }
         updateDisplayNumber(numberButtonText);
-    }
-
-    public void onOperatorButtonClicked(String operatorButtonText) {
-        String currentDisplayText = displayText.getValue();
-        if (currentDisplayText == null) {
-            return;
-        }
-        if (selectedOperator != Operator.NONE) {
-            updateSelectedOperator(operatorButtonText);
-            preformCalculationAndUpdateDisplayNumber();
-            return;
-        }
-        updateSelectedOperator(operatorButtonText);
-        storedNumber = Double.parseDouble(displayText.getValue());
-    }
-
-    public void onEqualsButtonClicked() {
-        preformCalculationAndUpdateDisplayNumber();
-    }
-
-    public void onDotButtonClicked() {
-        appendDotToDisplayNumber();
-    }
-
-    public void onSignButtonClicked() {
-        changeSignOfDisplayNumber();
     }
 }
